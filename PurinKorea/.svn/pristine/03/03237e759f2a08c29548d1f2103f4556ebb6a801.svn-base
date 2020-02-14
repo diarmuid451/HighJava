c49737ea6cda073c9b357c47fb24aad7c68e0f5e
diarmuid <diarmuid451@gmail.com>
2020-02-14 16:13:39 +0900

    ddd

---
+++ PurinKorea/.svn/pristine/03/03237e759f2a08c29548d1f2103f4556ebb6a801.svn-base
@@ -0,0 +1,7 @@
+package kr.or.ddit.svn;
+
+public class SvnTest {
+	public static void main(String[] args) {
+		System.out.println("우헤헤헤 테스트");
+	}
+}
---
+++ PurinKorea/src/kr/or/ddit/svn/SvnTest.java
@@ -0,0 +1,8 @@
+package kr.or.ddit.svn;
+
+public class SvnTest {
+	public static void main(String[] args) {
+		System.out.println("우헤헤헤 테스트");
+		System.out.println("우헤헤헤 테스트");
+	}
+}
